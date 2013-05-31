55c733febee8a356da573a08855ec6a7670b3da0
Christian Johansen <christian@gitorious.com>
2013-05-31 12:53:17 +0200

    Don't require stuff we don't use

--- lib/libdolt/repo_actions.rb
+++ lib/libdolt/repo_actions.rb
@@ -15,7 +15,6 @@
 #   You should have received a copy of the GNU Affero General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #++
-require "eventmachine"
 
 # Need consistent Time formatting in JSON
 require "time"

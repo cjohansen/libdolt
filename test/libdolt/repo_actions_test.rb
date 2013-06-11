e4f2bec982eb306096db55de23c98953d1a02466
Marius Mathiesen <marius@gitorious.com>
2013-06-11 15:03:10 +0200

    Run repo_actions_test against a real repo
    test/fixtures/dolt-test-repo.git contains a real Git repository, we'll
    use this for testing.

--- test/libdolt/repo_actions_test.rb
+++ test/libdolt/repo_actions_test.rb
@@ -20,38 +20,13 @@ require "libdolt/repo_actions"
 require "ostruct"
 require "mocha/setup"
 
-class Repository
-  attr_reader :name
-  def initialize(name)
-    @name = name
-  end
-
-  def tree(ref, path); end
-  def tree_entry(ref, path); end
-  def rev_parse(rev); end
-  def rev_parse_oid(ref); self.class.refs[ref] || nil; end
-  def blame(ref, path); end
-  def log(ref, path, limit); end
-  def blob(ref, path); end
-  def refs; end
-  def tree_history(ref, path, count); end
-  def readmes(ref, path); []; end
-
-  def self.stub_ref(name, ref)
-    self.refs[name] = ref
-  end
-
-  def self.refs
-    @refs ||= {}
-  end
-end
-
 class Resolver
   attr_reader :resolved
   def initialize; @resolved = []; end
 
   def resolve(repo)
-    repository = Repository.new(repo)
+    dot_git = File.dirname(__FILE__) + "/../fixtures/dolt-test-repo.git"
+    repository = Dolt::Git::Repository.new(dot_git)
     @resolved << repository
     repository
   end
@@ -67,209 +42,101 @@ class MetaResolver < Resolver
   end
 end
 
-class FakeBlob; end
-
 describe Dolt::RepoActions do
   before do
     @resolver = Resolver.new
     @actions = Dolt::RepoActions.new(@resolver)
-    @blob = FakeBlob.new
-    @tree = { :type => :tree }
-    @blame = { :type => :blame }
   end
 
   describe "#blob" do
-    it "resolves repository" do
-      @actions.blob("gitorious", "master", "app")
-
-      assert_equal ["gitorious"], @resolver.resolved.map(&:name)
-    end
-
     it "returns path, blob, repo, ref and base_tree_url" do
-      Repository.any_instance.stubs(:rev_parse).returns(@blob)
-      data = @actions.blob("gitorious", "babd120", "app")
-
-      assert_equal({
-          :blob => @blob,
-          :repository_slug => "gitorious",
-          :ref =>  "babd120",
-          :path => "app"
-        }, data)
+      data = @actions.blob("gitorious", "fc5f5fb50b435e18", "lib/foo.rb")
+      assert_equal "gitorious", data[:repository_slug]
+      assert_equal "fc5f5fb50b435e18", data[:ref]
+      assert Rugged::Blob === data[:blob]
     end
   end
 
   describe "#tree" do
-    it "resolves repository" do
-      @actions.tree("gitorious", "master", "app")
-
-      assert_equal ["gitorious","gitorious"], @resolver.resolved.map(&:name)
-    end
-
     it "returns tree, repo and ref" do
-      Repository.any_instance.stubs(:tree).returns(@tree)
-      data = @actions.tree("gitorious", "babd120", "app")
+      data = @actions.tree("gitorious", "fc5f5fb50b435e18", "lib")
       repo = @resolver.resolved.last
-
-      assert_equal({
-          :tree => @tree,
-          :repository_slug => "gitorious",
-          :ref =>  "babd120",
-          :path => "app",
-          :readme => nil
-        }, data)
+      assert_equal "264c348a80906538018616fa16fc35d04bdf38b0", data[:tree].oid
+      assert_equal "fc5f5fb50b435e18", data[:ref]
+      assert_equal "lib", data[:path]
     end
   end
 
   describe "readmes" do
     it "includes readmes which can be rendered" do
       readme_name = "README.org"
-      Repository.any_instance.stubs(:tree).returns(@tree)
-      readmes = [{:name => readme_name, :blob => nil}]
-      Repository.any_instance.stubs(:readmes).with("fc00ff","lib").returns(readmes)
       Makeup::Markup.stubs(:can_render?).with(readme_name).returns(true)
-      data = @actions.tree("gitorious","fc00ff","lib")
+      data = @actions.tree("gitorious","fc5f5fb50b435e18","")
       repo = @resolver.resolved.last
-      assert_equal "lib/#{readme_name}", data[:readme][:path]
+      assert_equal "#{readme_name}", data[:readme][:path]
     end
   end
 
   describe "#tree_entry" do
     it "returns tree, repo and ref" do
-      Repository.any_instance.stubs(:tree_entry).returns(@tree)
-      data = @actions.tree_entry("gitorious", "babd120", "")
+      data = @actions.tree_entry("gitorious", "fc5f5fb50b435e18", "")
       repo = @resolver.resolved.last
-
-      assert_equal({
-          :tree => @tree,
-          :repository_slug => "gitorious",
-          :ref =>  "babd120",
-          :path => "",
-          :type => :tree
-        }, data)
+      assert_equal :tree, data[:type]
     end
 
     it "returns blob, repo and ref" do
-      Repository.any_instance.stubs(:tree_entry).returns(@blob)
-      data = @actions.tree_entry("gitorious", "babd120", "Gemfile")
+      data = @actions.tree_entry("gitorious", "fc5f5fb50b435e18", "lib/foo.rb")
 
-      assert_equal({
-          :blob => @blob,
-          :repository_slug => "gitorious",
-          :ref =>  "babd120",
-          :path => "Gemfile",
-          :type => :blob
-        }, data)
+      assert_equal "lib/foo.rb", data[:path]
+      assert_equal "fc5f5fb50b435e18", data[:ref]
+      assert_equal :blob, data[:type]
     end
   end
 
   describe "#blame" do
     it "resolves repository" do
-      @actions.blame("gitorious", "master", "app")
-
-      assert_equal ["gitorious"], @resolver.resolved.map(&:name)
+      @actions.blame("gitorious", "master", "lib")
+      assert_equal 1, @resolver.resolved.size
     end
 
     it "returns blame, repo and ref" do
-      Repository.any_instance.stubs(:blame).returns(@blame)
-      data = @actions.blame("gitorious", "babd120", "app")
-
-      assert_equal({
-          :blame => @blame,
-          :repository_slug => "gitorious",
-          :ref =>  "babd120",
-          :path => "app"
-        }, data)
+      data = @actions.blame("gitorious", "fc5f5fb50b435e18", "lib")
+      assert Dolt::Git::Blame === data[:blame]
+      assert_equal "gitorious", data[:repository_slug]
+      assert_equal "fc5f5fb50b435e18", data[:ref]
+      assert_equal "lib", data[:path]
     end
   end
 
   describe "#history" do
-    it "resolves repository" do
-      @actions.history("gitorious", "master", "app", 1)
-
-      assert_equal ["gitorious"], @resolver.resolved.map(&:name)
-    end
-
     it "returns commits, repo and ref" do
-      Repository.any_instance.stubs(:log).returns([])
-      data = @actions.history("gitorious", "babd120", "app", 2)
+      data = @actions.history("gitorious", "fc5f5fb50b435e18", "app", 2)
 
       assert_equal({
           :commits => [],
           :repository_slug => "gitorious",
-          :ref =>  "babd120",
+          :ref =>  "fc5f5fb50b435e18",
           :path => "app"
         }, data)
     end
   end
 
   describe "#refs" do
-    before do
-      @refs = ["refs/stash",
-               "refs/tags/v0.2.1",
-               "refs/tags/v0.2.0",
-               "refs/remotes/origin/master",
-               "refs/heads/libgit2",
-               "refs/heads/master"].map { |n| OpenStruct.new(:name => n) }
-    end
-
     it "returns repositories, tags and heads" do
-      Repository.any_instance.stubs(:refs).returns(@refs)
-      Repository.stub_ref("refs/tags/v0.2.0", "a" * 40)
-      Repository.stub_ref("refs/tags/v0.2.1", "b" * 40)
-      Repository.stub_ref("refs/heads/libgit2", "c" * 40)
-      Repository.stub_ref("refs/heads/master", "d" * 40)
-
       data = @actions.refs("gitorious")
 
-      assert_equal({
-          :repository_slug => "gitorious",
-          :heads => [["libgit2", "c" * 40], ["master", "d" * 40]],
-          :tags => [["v0.2.1", "b" * 40], ["v0.2.0", "a" * 40]]
-        }, data)
+      assert data[:tags].detect {|name, ref|
+        name == "testable-tag" && ref == "fc5f5fb50b435e183925b341909610aace90a413"
+      }
     end
   end
 
   describe "#tree_history" do
-    before do
-      @tree = [{
-          :type => :blob,
-          :oid => "e90021f89616ddf86855d05337c188408d3b417e",
-          :filemode => 33188,
-          :name => ".gitmodules",
-          :history => [{
-            :oid => "906d67b4f3e5de7364ba9b57d174d8998d53ced6",
-            :author => { :name => "Christian Johansen",
-                         :email => "christian@cjohansen.no" },
-            :summary => "Working Dolt server for viewing blobs",
-            :date => Time.parse("Mon Sep 10 15:07:39 +0200 2012"),
-            :message => ""
-          }]
-        }, {
-          :type => :blob,
-          :oid => "c80ee3697054566d1a4247d80be78ec3ddfde295",
-          :filemode => 33188,
-          :name => "Gemfile",
-          :history => [{
-            :oid => "26139a3aba4aac8cbf658c0d0ea58b8983e4090b",
-            :author => { :name => "Christian Johansen",
-                         :email => "christian@cjohansen.no" },
-            :summary => "Initial commit",
-            :date => Time.parse("Thu Aug 23 11:40:39 +0200 2012"),
-            :message => ""
-          }]
-        }]
-    end
-
     it "returns repository, path, ref and history" do
-      Repository.any_instance.stubs(:tree_history).returns(@tree)
-      data = @actions.tree_history("gitorious", "master", "", 1)
-
-      assert_equal({
-          :repository_slug => "gitorious",
-          :ref => "master",
-          :path => "",
-          :tree => @tree
-        }, data)
+      data = @actions.tree_history("gitorious", "testable-tag", "", 1)
+      assert_equal "testable-tag", data[:ref]
+      assert_equal 2, data[:tree].length
+      assert_equal "", data[:path]
     end
   end
 
@@ -277,7 +144,7 @@ describe Dolt::RepoActions do
     it "is returned with other data" do
       resolver = MetaResolver.new
       actions = Dolt::RepoActions.new(resolver)
-      data = actions.blob("gitorious", "babd120", "app")
+      data = actions.blob("gitorious", "fc5f5fb50b435e18", "lib")
 
       assert_equal "Meta data is cool", data[:repository_meta]
     end
@@ -285,9 +152,8 @@ describe Dolt::RepoActions do
 
   describe "#rev_parse_oid" do
     it "resolves ref oid" do
-      oid = "a" * 40
-      Repository.any_instance.stubs(:rev_parse_oid).returns(oid)
-      assert_equal oid, @actions.rev_parse_oid("gitorious", "master")
+      oid = "fc5f5fb50b435e183925b341909610aace90a413"
+      assert_equal oid, @actions.rev_parse_oid("gitorious", "testable-tag")
     end
  end
 end
